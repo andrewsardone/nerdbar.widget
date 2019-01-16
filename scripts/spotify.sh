f22b703b9a28fc99086f77bc071bf84a55fd7b59
Andrew Sardone <andrew@andrewsardone.com>
2019-01-16 13:58:21 -0500

    REBASE mE spotify playback crib spotify playback script

---
+++ scripts/spotify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+RESULTX="$(/usr/local/bin/spotify status 2>/dev/null; echo x)"
+RESULT="${RESULTX%x}"
+
+# echo "$(echo "$RESULT" | grep "Artist" | awk -F":" '{print $2}')@$(echo "$RESULT" | grep "Track:" | awk -F":" '{print $2}')@$(echo "$RESULT" | grep "Position:" | awk -F" " '{print $2}')@$(echo "$RESULT" | grep "Position:" | awk -F" " '{print $4}')@$(echo "$RESULT" | grep 'Spotify is currently' | awk -F" " '{print $4}')"
+
+echo "$(echo "$RESULT" | grep "Artist" | awk -F":" '{print $2}') – $(echo "$RESULT" | grep "Track:" | awk -F":" '{print $2}') ($(echo "$RESULT" | grep "Position:" | awk -F" " '{print $2}')/$(echo "$RESULT" | grep "Position:" | awk -F" " '{print $4}'))"
